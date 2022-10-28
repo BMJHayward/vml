53170bb60bcfdc45ae03d27d738a55010e8772ea
Brendan Hayward <email>
2022-10-28 21:48:32 +1100

    create more activation and loss functions with derivatives

--- nnet.v
+++ nnet.v
@@ -4,29 +4,74 @@ import arrays
 import math
 
 pub fn name() string {
-    return 'neural networks'
+    // TODO: single layer perception
+    // TODO: multilayer perception
+    // TODO: backpropagation, stochastic gradient descent
+    // TODO: other activate functs e.g. sigmoid, softmax
+    return 'basic feed forward neural network'
 }
 
 type FLoatFunc = fn (f64) f64
 
 pub struct NeuralNetModel {
 mut:
-	layers []int
-	activation  fn (f64) f64
-	error       fn ([]f64, []f64) f64
+	shape []int
+	layers []string
+	activation  fn ([]f64) []f64
+	error       fn ([]f64, []f64) []f64
 	backprop    bool
 }
 
-fn relu(input f64) f64 {
-    return arrays.max([0.0, input]) or { 0 }
+// Activation Functions
+fn relu(input []f64) []f64 {
+    return input.map(arrays.max([0.0, it]) or { 0 })
 }
 
-fn mse(target []f64, actual []f64) f64 {
+fn tanh(x []f64) []f64 {
+    return x.map(math.tanh(it))
+}
+
+fn d_tanh(x []f64) []f64 {
+    return (x.map(math.tanh(it))).map(math.pow(it, 2)).map(1 - it)
+}
+
+fn sigmoid(x []f64) []f64 {
+    return x.map(1/(1 + math.exp(-it)))
+}
+
+fn d_sigmoid(x []f64) []f64 {
+    mut sg := sigmoid(x)
+    return sg.map((1 - it) * it)
+}
+
+// Loss Functions 
+fn logloss(y []f64, a []f64) []f64 {
+    // asdf
+    ya_range := math.min(y.len, a.len)
+    alog := a.map(math.log(it))
+    alogit := a.map(math.log(1-it))
+    mut lgl := []f64{}
+    for i in 0 .. ya_range {
+        lgl << -1 * (y[i] * alog[i] + (1 - y[i]) * alogit[i])
+    }
+    return lgl
+}
+
+fn d_logloss(y []f64, a []f64) []f64 {
+    mut ay := []f64{len: math.min(y.len, a.len)}
+    for i in 0 .. int(math.min(y.len, a.len)) {
+        ay << a[i] - y[i]
+    }
+    mut ll := a.map(it*(1 - it))
+    return arrays.group<f64>(ay, ll).map(it[0] / it[1])
+}
+
+fn mse(target []f64, actual []f64) []f64 {
     mut sqerr := []f64{}
     for i in 0 .. actual.len {
         sqerr << math.pow(target[i] - actual[i], 2.0)
     }
-    return arrays.sum(sqerr) or { 0 } / sqerr.len
+    return [arrays.sum(sqerr) or { 0 } / sqerr.len]
 }
 
 pub fn (mut m NeuralNetModel) train<T>(inputs [][]T, output []T, iterations int) []NeuralNetModel {
@@ -41,6 +86,7 @@ pub fn (mut m KMeansModel) predict<T>(data [][]T) ![]f64 {
 pub fn demo() ![]NeuralNetModel {
 	return [NeuralNetModel{
         [3,3,3]
+        ['relu', 'relu', 'relu']
         relu
         mse
         false
