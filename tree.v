fed683796f50d4d55d02b7ebd353b51d802b6fd6
Brendan Hayward <brendanhayward85@gmail.com>
2022-11-25 20:32:41 +1100

    fix returns in tree

--- tree.v
+++ tree.v
@@ -1,20 +1,24 @@
 module tree
 
+import arrays
+import math
+
 pub fn name() string {
     return 'decision tree'
 }
 
 fn entropy(y []u8) f64 {
-    mut hist := []u8{len: arrays.max(y) + 1, init: 0} 
+    ymax := arrays.max(y) or { panic('max y failed')}
+    mut hist := []u8{len: int(ymax) + 1, init: 0} 
     for i in 0 .. y.len {
         hist[y[i]] += 1
     }
     mut probs := hist.map(it / y.len)
-    return arrays.sum(probs.filter(it > 0).map(it * log2(it)))
+    return arrays.sum(probs.filter(it > 0).map(it * math.log2(it))) or { panic('failed to sum array') }
 }
 
 fn accuracy(y_true []f64, y_pred []f64) f64 {
-  mut acc = 0.0
+  mut acc := 0.0
   for t in 0 .. y_true.len {
     if y_true[t] == y_pred[t] {
         acc += 1
@@ -23,16 +27,20 @@ fn accuracy(y_true []f64, y_pred []f64) f64 {
   return acc / y_true.len
 }
 
-type featureType = []f64 | []int | []string
+type Feature = []f64 | []int | []string
+type Tree = Empty | Node
+
+struct Empty {}
+
 struct Node {
-    feature featureType
+    feature Feature
     threshold f64
-    left Node
-    right Node
+    left Tree
+    right Tree
     value f64
 }
 
-fn _initNode(feature featureType, threshold f64 | string, left Node, right Node, value f64) Node {
+fn init_node(feature Feature, threshold f64, left Node, right Node, value f64) Tree {
     return Node {
         feature
         threshold
@@ -42,17 +50,18 @@ fn _initNode(feature featureType, threshold f64 | string, left Node, right Node,
     }
 }
 
-struct DecisionTree:
-        min_samples_split int
-        max_depth int
-        n_feats int
-        root Node
+struct DecisionTree {
+    min_samples_split int
+    max_depth int
+    n_feats int
+    root Tree
+}
 
-fn (dt DecisionTree) _init_tree(min_samples_split=2, max_depth=100, n_feats=0) {
+fn init_tree(min_samples_split int, max_depth int, n_feats int) DecisionTree {
     return DecisionTree {
         min_samples_split
         max_depth
         n_feats
-        _init_Node()
+        Empty{}
     }
 }
