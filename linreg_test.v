7c49ffb4824dd09e61021e3158f1601c34b67980
Brendan Hayward <brendanhayward85@gmail.com>
2023-01-17 16:19:24 +1100

    add more tests to linear regression

--- linreg_test.v
+++ linreg_test.v
@@ -29,16 +29,25 @@ fn test_estimate_coefficients() {
 }
 
 fn test_train() {
-    inputs := [[1,2,3], [4,5,6], [7,8,9]]
-    outputs := [11, 12, 13]
-    linreg.train(inputs, outpus)
+    inputs := [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
+    outputs := [11.0, 12.0, 13.0]
+    test_runner := linreg.LinearModel{ coeffs: []}
+    test_model := test_runner.train(inputs, outputs)
+    println(test_model)
     assert 'test_train' == 'test_train'
 }
 
 fn  test_predict() {
-    assert 'test_predict' == 'test_predict'
+    test_model := linreg.LinearModel{
+        coeffs: [2.0, 2.0]
+    }
+    test_data := [1.0, 2.0, 3.0, 4.0, 5.0]
+    test_preds := test_model.predict(test_data)
+    assert test_preds == [4.0, 6.0, 8.0, 10.0, 12.0]
 }
 
 fn test_demo() {
+    test_demo := linreg.demo()
+    println(test_demo)
     assert 'test_demo' == 'test_demo'
 }
