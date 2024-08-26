46ec64eed574fed5ed7c11fa6ff6415086da91b4
Brendan Hayward <brendanhayward85@gmail.com>
2024-08-26 17:04:24 +1000

    add initial cli options

--- vml.v
+++ vml.v
@@ -8,16 +8,37 @@ import kmean
 import linreg
 import nnet
 import tree
+import os
+import flag
 
 fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application('vml')
+	fp.version('0.0.1')
+	fp.description('simple machine learning for the V programming language')
+	fp.skip_executable()
+
+	model := fp.string('model', `m`, 'none', 'The model to analyse your data.').to_lower()
+	demo := fp.string('demo', `e`, 'none', 'Demonstrate model training and output on fake data.').to_lower()
+	data := fp.string('data', `d`, 'none', 'Input data as csv file.').to_lower()
+	additional_args := fp.finalize() !
+
+	if additional_args.len > 0 {
+		println('Unprocessed arguments:\n$additional_args.join_lines()')
+	}
+
+	println(model)
+	println(demo)
+	println(data[0..data.len-1])
+
 	println('Hello VML!')
 	println('Loaded: $forest.name()')
 	println('Loaded: $kmean.name()')
 	// println('Loaded: $knn.name()')
 	println('Loaded: $linreg.name()')
-	//   println('Loaded: $logreg.name()')
+	// println('Loaded: $logreg.name()')
 	println('Loaded: $nnet.name()')
-	//    println('Loaded: $reinf.name()')
+	// println('Loaded: $reinf.name()')
 	println('Loaded: $tree.name()')
 
 	lrr := linreg.demo()
